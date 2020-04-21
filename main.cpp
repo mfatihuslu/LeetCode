49d07701ec91e3e6441e2083712488a709af32c2
Mehmet Fatih USLU <mfatihuslu@gmail.com>
2020-04-21 23:36:08 +0300

    solution of single number

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        int num = 0;
+        for(int i=0;i<nums.size();i++){
+
+            num = num^nums[i];
+        }
+        return num;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Solution S;
+    vector<int> v = {2,2,1};
+    std::cout << S.singleNumber(v) << std::endl;
+
     return 0;
 }
